744cd851e5c280070d3c289427878eb9d4ec9d65
iqbal-elham <iqbal.elham7@gmail.com>
2023-05-11 00:10:18 +0430

    Add decode_word method

--- main.rb
+++ main.rb
@@ -3,3 +3,15 @@ require_relative 'decoder'
 def decode_character character
    ALL_MORSE_CODE[character]
 end
+
+def decode_word(word)
+   word_arr = word.split
+   compound = ''
+ 
+   word_arr.each do |char|
+     compound += decode_character(char)
+   end
+ 
+   compound
+ end
+ 
